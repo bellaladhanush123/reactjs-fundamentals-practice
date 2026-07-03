bb963de994ea3f0903dcb51f930f493c55962834
Bellala Venkata Dhanush <dhanushbellala@gmail.com>
2026-07-03 12:10:10 +0530

    Added Profile Card code

---
+++ 01-JSX/Profile-Card/app.jsx
@@ -0,0 +1,37 @@
+import {useState} from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Profile Card</h1>
+      <hr/>
+
+      <h2> Name : Bellala Venkata Dhanush</h2>
+
+      <p>Role : Frontend Developer</p>
+
+      <p> Skills: </p>
+    <ul>
+        <li> HTML </li>
+        <li> CSS </li>
+        <li> Javascript </li>
+        <li> REACT </li>
+    </ul> 
+      
+
+<p>Email:
+bellaladhanush123@gmail.com</p>
+
+<p> Phone:
+9876543210</p>
+
+<button> Contact Me</button>
+
+
+
+     
+    </div>
+  );
+}
+
+export default App;
