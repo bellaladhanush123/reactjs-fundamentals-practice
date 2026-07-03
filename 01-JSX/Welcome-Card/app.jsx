4ffd7ba0f300cc36413887adeccc04be80438a38
Bellala Venkata Dhanush <dhanushbellala@gmail.com>
2026-07-03 12:28:16 +0530

    Added Welcome Card code

---
+++ 01-JSX/Welcome-Card/app.jsx
@@ -0,0 +1,19 @@
+import {useState} from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+
+      <p> Name : Bellala Venkata Dhanush</p>
+
+      <p>Role : Frontend Developer</p>
+
+      <p>College : Malla Reddy University</p>
+
+      <button>Explore Projects</button>
+    </div>
+  );
+}
+
+export default App;
